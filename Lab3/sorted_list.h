0be95e9c9b9f6217914099f58f679e71d592c94e
Jacob Lager <jacla554@student.liu.se>
2022-11-02 22:07:28 +0100

    Added some documentation and tried to kill myself after thinking about Ohm's Law

--- Lab3/sorted_list.h
+++ Lab3/sorted_list.h
@@ -13,20 +13,28 @@ private:
         Node *next{};
     };
     Node *first{};
+    // Recursive helper function for insert
     void insert_sort(Node *current_node, Node *temp);
-    void recursive_print(Node* current, string &str);
+    // Recursive helper function for print
+    void recursive_print(Node *current, string &str);
 
 public:
+    // Default constructor
     Sorted_List() = default;
 
+    // Desctructor
     ~Sorted_List();
 
+    // Move constructor
     Sorted_List(Sorted_List &&original);
 
+    // Copy constructor
     Sorted_List(Sorted_List const &original);
 
+    // Copy operator
     Sorted_List &operator=(Sorted_List const &original);
 
+    // Move operator
     Sorted_List &operator=(Sorted_List &&original);
 
     void insert(int const &data);
