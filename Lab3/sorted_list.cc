9fb2a71511f4315ef7766e70495015ba28aba7b3
Jacob Lager <jacla554@student.liu.se>
2022-11-02 21:50:05 +0100

    Added some documentation and tried to kill myself after thinking about Ohm's Law

--- Lab3/sorted_list.cc
+++ Lab3/sorted_list.cc
@@ -10,6 +10,7 @@ Sorted_List::~Sorted_List()
     clear_list();
 }
 
+// Move constructor
 Sorted_List::Sorted_List(Sorted_List &&original)
 {
     *this = move(original);
@@ -24,15 +25,15 @@ Sorted_List::Sorted_List(Sorted_List const &original)
 // Copy operator
 Sorted_List &Sorted_List::operator=(Sorted_List const &original)
 {
-   if (original.first != nullptr)
+    if (original.first != nullptr)
     {
-        // *original_node points at the value from the original list, so we can "get" the value
-        // *copied_node points at a new node which acts as a copy, with values from the "getter" original_node
+        // *original_node points to the first value from the original list, so that we can "get" the value
+        // *copied_node points to a new node which acts as a copy, with values from the "getter" original_node
         Node *original_node{original.first};
         Node *copied_node{new Node{original_node->data, nullptr}};
-        
-        //copy lists first node gets the first value from the original list
-        //we change the original_node to the next one in the original list, even if its NULL
+
+        // 'first' node gets the first value from the original list
+        // we change the original_node to the next one in the original list, even if its NULL
         first = copied_node;
         original_node = original_node->next;
 
@@ -69,18 +70,18 @@ void Sorted_List::insert(int const &data)
 
     Node *new_node{new Node{data, nullptr}};
 
-    //if empty
+    // if empty, place the new node in the first position
     if (first == nullptr)
     {
         first = new_node;
     }
-    //if new_node data is lower than the first node data, assign the new_node as first
+    // if new_node data is lower than the first node data, assign the new_node as first
     else if (data < first->data)
     {
         new_node->next = first;
         first = new_node;
     }
-    //find correct position
+    // else, find correct position in the list to "place" the new node
     else
     {
         insert_sort(first, new_node);
@@ -89,25 +90,25 @@ void Sorted_List::insert(int const &data)
 
 void Sorted_List::insert_sort(Node *current_node, Node *new_node)
 {
-    //if the current_node is the last, connect the new_node as the last node in list
+    // if the current_node is the last node, connect the new_node as the last node in list
     if (current_node->next == nullptr)
     {
         current_node->next = new_node;
         new_node->next = nullptr;
         return;
     }
-    
+
     Node *next_node{current_node->next};
-    //if the upcoming nodes data is higher, then should new_node be between current and currents next
-    //current_node -> new_node -> next_node
-    //can't use next_node = new_node, because then we cant use the "primary" next_node for new_node->next = next_node
+    // if next_node->data is higher than or equal to new_node->data, then new_node should be added "between" current_node and next_node
+    // illustration: current_node -> new_node -> next_node
+    // can't use next_node = new_node, because then we cant use the "primary" next_node for new_node->next = next_node
     if (new_node->data <= next_node->data)
     {
         current_node->next = new_node;
         new_node->next = next_node;
         return;
     }
-    //keep searching
+    // else, keep searching for the right position
     else
     {
         insert_sort(next_node, new_node);
@@ -118,7 +119,7 @@ void Sorted_List::remove(int const &index)
 {
     Node *current{first};
 
-    //if the we are removing first, delete and replace it with second node as first
+    // if we are removing the first node, delete and replace it with the second node as first
     if (index == 0)
     {
         first = current->next;
@@ -126,17 +127,18 @@ void Sorted_List::remove(int const &index)
     }
     else
     {
-        Node*previous{first};
+        Node *previous{first};
         for (int i{0}; i < size(); ++i)
         {
-            //if correct index, connect the previous and the next together, then remove current
-            //previous -> [deleted current] -> current.next
+            // if correct index, connect the previous and the next together, then remove current node
+            // illustration: previous -> [deleted current] -> current->next
             if (i == index)
             {
                 previous->next = current->next;
                 delete current;
                 break;
             }
+            // else, update previous and current, and keep iterating until the right index is found
             else
             {
                 previous = current;
@@ -159,12 +161,16 @@ string Sorted_List::print()
     return str;
 }
 
-void Sorted_List::recursive_print(Node* current, string &str)
+void Sorted_List::recursive_print(Node *current, string &str)
 {
+    // add data from the current node to the string
     str.append(to_string(current->data));
-    if (current->next == nullptr) {
+    // if there is no next node, return
+    if (current->next == nullptr)
+    {
         return;
-    } 
+    }
+    // add formatting to the string and check the next node
     str.append(" -> ");
     recursive_print(current->next, str);
 }
@@ -173,7 +179,7 @@ int Sorted_List::getValueAt(int const &index) const
 {
     if (!is_empty())
     {
-        Node *tmp = first;
+        Node *tmp{first};
         for (int i{0}; i < index; ++i)
         {
             tmp = tmp->next;
