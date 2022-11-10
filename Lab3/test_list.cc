b832248ffb9d69539330d02d20aa65b5ffc1b950
Jacob Lager <jacob@jacobs-mbp.students.netlogon.liu.se>
2022-11-10 11:01:15 +0100

    Fixed comments for complementary hand-in

--- Lab3/test_list.cc
+++ Lab3/test_list.cc
@@ -1,17 +1,3 @@
-// This test program uses a test framework supporting TDD and BDD.
-// You are not required to use the framework, but encouraged to.
-// Code:
-// https://github.com/philsquared/Catch.git
-// Documentation:
-// https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
-
-// You ARE however required to implement all test cases outlined here,
-// even if you do it by way of your own function for each case.  You
-// are recommended to solve the cases in order, and rerun all tests
-// after you modify your code.
-
-// This define lets Catch create the main test program
-// (Must be in only one place!)
 #include "catch.hpp"
 #include "sorted_list.h"
 using namespace std;
