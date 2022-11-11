31f61ac3f6f88a679ff2bb8888c2453c01c13df1
Jacob Lager <jacla554@student.liu.se>
2022-11-11 15:57:12 +0100

    Tried to fix Simulation output. Failed miserably

--- Lab4/Simulation.cc
+++ Lab4/Simulation.cc
@@ -15,27 +15,38 @@ void Simulation::simulate(vector<Component *> const net, int const iterations, i
 {
     int size{static_cast<int>(net.size())};
     cout << endl;
-    for (int i{0}; i < size; ++i)
+    // for (int i{0}; i < size; ++i)
+    // {
+    //     cout << setw(11) << net.at(i)->getName();
+    // }
+    for (Component *element : net)
     {
-        cout << setw(12) << net.at(i)->getName();
+        cout << setw(12) << element->getName();
     }
     cout << endl;
     cout << left;
-    cout << setw(0) << "";
-    for (int i{0}; i < size; ++i) {
-        cout << "Volt  Curr  ";
+    cout << " ";
+    for (int i{0}; i < size; ++i)
+    {
+        cout << setw(6) << "Volt";
+        cout << setw(7) << "Curr";
     }
 
     cout << endl;
 
-    for (int i{0}; i <= iterations; ++i) {
-        for (Component* c : net) {
+    for (int i{0}; i <= iterations; ++i)
+    {
+        for (Component *c : net)
+        {
             c->update(timeStep);
         }
-        if (((i % (iterations / outputRows)) == 0) && (i != 0)) {
-            cout << fixed << setprecision(2) << setw(0) << "";
-            for (Component* c : net) {
-                cout << setw(6) << c->getVoltage() << c->getCurrent() << "  ";
+        if (((i % (iterations / outputRows)) == 0) && (i != 0))
+        {
+            cout << fixed << setprecision(2);
+            for (Component *c : net)
+            {
+                cout << setw(7) << c->getVoltage();
+                cout << setw(6) << c->getCurrent();
             }
             cout << endl;
         }
