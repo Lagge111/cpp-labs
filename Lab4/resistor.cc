99d9deaa5c7d282913d9099e44b976c9ec8ec80b
matla588 <matla588@student.liu.se>
2022-11-16 15:11:09 +0100

    mindre skit v.1 - nu med resistor!

--- Lab4/resistor.cc
+++ Lab4/resistor.cc
@@ -6,37 +6,33 @@
 using namespace std;
 
 Resistor::Resistor(string const name, double const resistance, Connection *const left, Connection *const right)
-    : Component(name, left, right), resistance{resistance}
+    : Component(name, left, right), resistance{resistance}, current{0}
 {
 }
 
-// double Resistor::getVoltage()
-// {
-//     // voltage is undefined
-//     return voltage;
-// }
-
 void Resistor::update(double const timeStep)
 {
-    double voltageMoved{(getVoltage() / resistance) * timeStep};
+    double movedCharge{(getVoltage() / resistance) * timeStep};
 
     if (left->getVoltage() < right->getVoltage())
-    {
-        left->setVoltage(left->getVoltage() + voltageMoved);
-        right->setVoltage(right->getVoltage() - voltageMoved);
-    }
+        changeVoltage(left, right, movedCharge);
     else
-    {
-        right->setVoltage(right->getVoltage() + voltageMoved);
-        left->setVoltage(left->getVoltage() - voltageMoved);
-    }
+        changeVoltage(right, left, movedCharge);
+
+    current = getVoltage() / resistance;
 }
 
 double Resistor::getCurrent()
 {
-    return (getVoltage() / resistance);
+    return current;
 }
 
 Resistor::~Resistor()
 {
+}
+
+void Resistor::changeVoltage(Connection *lowest, Connection *highest, double movedCharge)
+{
+    lowest->setVoltage(lowest->getVoltage() + movedCharge);
+    highest->setVoltage(highest->getVoltage() - movedCharge);
 }
