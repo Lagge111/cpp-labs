f4e7429ae0a8d104026b25bbba00e5ce134b36bd
Jacob Lager <jacla554@student.liu.se>
2022-11-21 09:51:41 +0100

    Added given files

--- Lab5/main.cc
+++ Lab5/main.cc
@@ -1,8 +1,15 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+ifstream infile{};
+
 int main(int argc, char **argv)
 {
+    if (argc > 1)
+    {
+        infile.open(argv[1]);
+    }
 }
